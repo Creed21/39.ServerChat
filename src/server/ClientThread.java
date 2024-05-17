09f06835746f848ac9256e5601ae3612cb77c8bc
Creed21 <aleksandar.jankovic95@yahoo.com>
2024-05-17 20:15:52 +0200

    [REFACTOR] - deleting extra code

--- src/server/ClientThread.java
+++ src/server/ClientThread.java
@@ -4,20 +4,11 @@ import java.io.*;
 import java.net.Socket;
 
 public class ClientThread extends Thread {
-    private Socket clientSocket;
+    private final Socket clientSocket;
     private BufferedReader reader;
     private PrintWriter writer;
     private BufferedReader keyboard;
 
-    /**
-     * ****** napomena ******
-     *      AKO NA SERVERSKOJ STRANI PORVO INICIJAZIUJETE
-     *
-     *      BufferedReader pa onda PrintWriter
-     *      NA KLIJENTSKOJ STRANI MORA BITI OBRNUTO
-     *
-     *      PrintWriter pa onda BufferedReader
-     */
     public ClientThread(Socket clientSocket) {
         this.clientSocket = clientSocket;
         try {
@@ -29,41 +20,32 @@ public class ClientThread extends Thread {
         }
     }
 
-    /**
-     * ovde se nalazi kod koji se izvrsava za klijentsku nit
-     * => proces (kao mini program)
-     */
     @Override
     public void run() {
         handleCommunication();
     }
 
     private void handleCommunication() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            System.out.println("Uspavao sam metodu cisto da prodje inicijalizacija I/O streamova (similacija cekanja)");
-        }
-
-        try {
-            // poruka koju klijent salje
-            String msgFromClient = reader.readLine();
+        while (true) {
+            String msgFromClient = readMsgFromClient();
             System.out.println("Msg from client: " + msgFromClient);
-            // poruka koju klijent salje - zapamcena
 
-            // send successful reading status
-            sendMsg("Msg read!");
-
-            // enter message from keyboard and send to client
             sendMsgFromKeyboard();
+        }
+    }
+
+    private String readMsgFromClient() {
+        try {
+            return reader.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
     private void sendMsgFromKeyboard() {
         try {
-            System.out.print("Enter message for server: ");
+            System.out.print("Enter message for client: ");
             sendMsg(keyboard.readLine());
         } catch (IOException e) {
             e.printStackTrace();
